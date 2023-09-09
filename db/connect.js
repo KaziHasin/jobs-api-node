4e1cb01dc335c9dbeec8b7b3f2d39a6550095f39
KaziHasin <akashkazi012@gmail.com>
2023-09-09 09:04:41 +0530

    change in db connection

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,10 @@ require("dotenv").config();
 const connectionString = process.env.MONGO_URI;
 
 mongoose
-  .connect(connectionString)
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Mongo DB connected");
   })
